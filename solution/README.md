17bc18cdf7348dc0f5c6095da46a8b138f44765b
Murat Çelik <81585699+celikmurat1845@users.noreply.github.com>
2022-03-09 14:28:17 +0300

    fire-contact-project

---
+++ solution/README.md
@@ -0,0 +1,17 @@
+# FireContact Project
+
+FireContact is an app about phone contacts using firebase realtime database.
+
+## Project Gif
+
+![FireContact App](firecontact.gif)
+
+## .env file content
+
+REACT_APP_API_KEY=""  
+REACT_APP_AUTH_DOMAIN=""  
+REACT_APP_DATABASE_URL=""  
+REACT_APP_PROJECT_ID=""  
+REACT_APP_STORAGE_BUCKET=""  
+REACT_APP_MESSAGING_SENDER_ID=""  
+REACT_APP_APP_ID=""
---
+++ solution/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Form from "./components/form/Form";
+import Contacts from "./components/table/Contacts";
+import { ToastContainer } from "react-toastify";
+import { updateInfo, addInfo } from "./utils/functions";
+
+const initialValues = { username: "", phoneNumber: "", gender: "NO INFO!" };
+
+function App() {
+  const [info, setInfo] = useState(initialValues);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (info.id) {
+      updateInfo(info);
+    } else {
+      addInfo(info);
+    }
+    setInfo(initialValues);
+  };
+
+  const editHandler = (id, username, phoneNumber, gender) => {
+    setInfo({ id, username, phoneNumber, gender });
+  };
+
+  return (
+    <div className="App">
+      <Form
+        className="form"
+        info={info}
+        setInfo={setInfo}
+        handleFormSubmit={handleFormSubmit}
+      />
+      <Contacts className="contacts" editHandler={editHandler} />
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
